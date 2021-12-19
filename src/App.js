d5b4fcf667e5d77773722beeae7f6d2dce7ac188
Vanessa <vanessamrios@gmail.com>
2021-12-19 21:21:31 +0100

    (Req 01) Cria o componente Table

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Table from './components/Table';
 import PlanetsProvider from './PlanetsProvider';
 
 function App() {
   return (
     <PlanetsProvider>
       <div>Vanessa</div>
+      <Table />
     </PlanetsProvider>
   );
 }
---
+++ src/components/Table.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export default function Table() {
+  return (
+    <div>
+      <table border="1">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Rotation Period</th>
+            <th>Orbital Period</th>
+            <th>Diameter</th>
+            <th>Climate</th>
+            <th>Gravity</th>
+            <th>Terrain</th>
+            <th>Surface Water</th>
+            <th>Population</th>
+            <th>Films</th>
+            <th>Created</th>
+            <th>Edited</th>
+            <th>URL</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* { expenses.map((expense) => renderExpense(expense)) } */}
+        </tbody>
+      </table>
+    </div>
+  );
+}
